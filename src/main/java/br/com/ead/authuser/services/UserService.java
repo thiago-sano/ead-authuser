23a8792fcfac064dfbe1a48711e897da5ed310f0
thiago.sano <thiago.sano@outlook.com>
2025-05-19 08:41:00 -0300

    create controller

---
+++ src/main/java/br/com/ead/authuser/services/UserService.java
@@ -0,0 +1,16 @@
+package br.com.ead.authuser.services;
+
+import br.com.ead.authuser.models.UserModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserService {
+
+    List<UserModel> findAll();
+
+    Optional<UserModel> findById(UUID userId);
+
+    void delete(UserModel userModel);
+}
