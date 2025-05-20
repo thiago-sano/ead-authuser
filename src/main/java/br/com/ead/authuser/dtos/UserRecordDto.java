1f0828ed92a3761f7da19636d190533cea998b60
thiago.sano <thiago.sano@outlook.com>
2025-05-20 09:45:07 -0300

    create validation with @constraint

--- src/main/java/br/com/ead/authuser/dtos/UserRecordDto.java
+++ src/main/java/br/com/ead/authuser/dtos/UserRecordDto.java
@@ -1,5 +1,6 @@
 package br.com.ead.authuser.dtos;
 
+import br.com.ead.authuser.validations.PasswordConstraint;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -18,11 +19,13 @@ public record UserRecordDto(
 
         @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class}, message = "Password is mandatory")
         @Size(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class}, min = 6, max = 20, message = "Size mut be between 6 and 20")
+        @PasswordConstraint(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
         @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
         String password,
 
         @NotBlank(groups = UserView.PasswordPut.class, message = "Old password is mandatory")
         @Size(groups = UserView.PasswordPut.class, min = 6, max = 20, message = "Size mut be between 6 and 20")
+        @PasswordConstraint(groups = UserView.PasswordPut.class)
         @JsonView(UserView.PasswordPut.class)
         String oldPassword,
 
